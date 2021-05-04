d2b9ad726f85236cf1e5af8c2239429aa92207a9
Vladimir-Trufanov <tve@karelia.ru>
2021-05-04 14:03:38 +0300

    Контрольная точка перед созданием базы данных материалов

--- www/EdisiteArticle/_Edit/EdisiteArticle.php
+++ www/EdisiteArticle/_Edit/EdisiteArticle.php
@@ -1,13 +1,13 @@
 <?php
-// PHP7/HTML5, EDGE/CHROME                                 *** EditText.php ***
+// PHP7/HTML5, EDGE/CHROME                           *** EdisiteArticle.php ***
 
 // ****************************************************************************
-// * ittve.me                              Создать или редактировать материал *
+// *                     Создать или редактировать материал                   *
 // ****************************************************************************
 
 //                                                   Автор:       Труфанов В.Е.
 //                                                   Дата создания:  28.11.2020
-// Copyright © 2020 tve                              Посл.изменение: 28.11.2020
+// Copyright © 2020 tve                              Посл.изменение: 04.05.2021
 
 /**  ЗАМЕЧАНИЕ:
  *   --- Данный модуль для вывода картинки запускается из 2 мест: из галереи 
@@ -17,67 +17,78 @@
  * меняется режим страничного вывода на полноформатный и наоборот.
  * 
  * Передаваемые общесайтовые переменные
- *    $ImgdirSite="C:/IttveIMG/IttvePW/";  // каталог изображений сайта
- *    $PrefSite="itpw";                    // префикс изображений и статей сайта
-
-*/
-
+ * 
+ * $c_NameCharter                     - наименование раздела
+ * $c_NameArt                         - заголовок=имя файла редактируемой статьи
+ * $TranslitCharter                   - транслит наименования раздела
+ * $Translit                          - транслит заголовка редактируемой статьи
+ * $ImgdirSite="C:/IttveIMG/IttvePW/" - каталог изображений сайта
+ * $PrefSite="itpw"                   - префикс изображений и статей сайта
+ *    
+**/
 ?>
 <!-- 
 <form action="textarea1.php" method="post">
 -->
-   <div id="NewGallery">
-      <?php
-      require_once "GalleryNews.php";
-      ?>
-   </div>
-
-   <?php
-      // Создаем каталог для хранения изображений, если его нет.
-      // И отдельно (чтобы сработало на старых Windows) задаем права
-      $imgDir = "GalleryProba";
-      if (!is_dir($imgDir))
-      {
-         mkdir($imgDir);      
-         chmod($imgDir,0777);
-      }
-      // set the maximum upload size in bytes
-      $max = 57200;
-      //if (isset($_POST['upload'])) 
-      if (isset($_POST['upload'])) 
-      {     
-         // define the path to the upload folder
-         $destination = $imgDir.'/';
-         try 
-         {
-            $upload = new ttools\UploadToServer($destination);
-            $upload->move();
-            $upload->setMaxSize($max);
-            $result = $upload->getMessages();
-         } 
-         catch (Exception $e) 
-         {
-            echo $e->getMessage();
-         }
-      }
-   
-      echo '<p><textarea id="TitleArea" name="areat">Заголовок</textarea></p>';
-      echo '<div id="EditDebug">
-            <p><b>Создать материал или редактировать его</b></p>';
-      echo '<br>';
-      //prown\ViewGlobal(avgPOST);
-      
-      // Выводим сообщения по итогам загрузки файла
-      if (isset($result)) 
-      {
-         echo '<ul>';
-         foreach ($result as $message) 
-         {
-            echo "<li>$message</li>";
-         }
-         echo '</ul>';
-      }
-
+<?php
+// Обеспечиваем загрузку изображений для страницы сайта
+echo '<div id="NewGallery">';
+   // Подключаем загруженные картинки
+   require_once "GalleryNews.php";
+   // Готовим загрузку новой картинки
+   //echo '<form id="fGallery" action="'.$SpecSite.'">';
+   echo '<form action="" method="post" enctype="multipart/form-data" id="uploadiImage">';
+   $FileName="ittveNews/ittve01-001-С-заботой-и-к-мамам.jpg";
+   $Comment="'С заботой и к мамам' - такой мамочкин хвостик.";
+   GLoadImage($FileName,$Comment,true,"Upload");
+   echo '</form>';
+   // Из галереи задаем режим представления выбранной картинки - "на высоту страницы"
+   //$s_ModeImg=prown\MakeSession('ModeImg',vimOnPage,tInt);           
+echo '</div>';
+// Создаем каталог для хранения изображений, если его нет.
+// И отдельно (чтобы сработало на старых Windows) задаем права
+$imgDir = "EdisiteArticle/_Current";
+if (!is_dir($imgDir))
+{
+   mkdir($imgDir);      
+   chmod($imgDir,0777);
+}
+// Устанавливаем максимальный размер в байтах для загружаемого файла в форму
+$max = 57200;
+// Загружаем выбранный файл, если был запрос
+if (isset($_POST['upload'])) 
+{     
+   // define the path to the upload folder
+   $destination = $imgDir.'/';
+   try 
+   {
+      $upload = new ttools\UploadToServer($destination);
+      $upload->move();
+      $upload->setMaxSize($max);
+      $result = $upload->getMessages();
+   } 
+   catch (Exception $e) 
+   {
+      echo $e->getMessage();
+   }
+}
+// Воспроизводим разметку и подготовленные материалы   
+echo '<p><textarea id="TitleArea" name="areat">'.$c_NameArt.'</textarea></p>';
+echo '<div id="EditDebug">';
+require $c_NameArt.".html";
+//prown\ViewGlobal(avgPOST);
+// Выводим сообщения по итогам загрузки файла
+/*
+if (isset($result)) 
+{
+   echo '<ul>';
+   foreach ($result as $message) 
+   {
+      echo "<li>$message</li>";
+   }
+   echo '</ul>';
+}
+*/
             ?>
             
             <!-- 
@@ -94,18 +105,21 @@
             </p>
             </form>
             -->
+            
+            
             <?php
             
-       echo '</div>';
-       echo '<p><textarea id="ContentArea" name="areac">Текст нового материала';
-       prown\ViewGlobal(avgREQUEST);
-       echo '</textarea></p>';
-   ?>
-   <p><input id="InputArea" type="submit" value="Отправить"></p>
+echo '</div>';
+// Заполняем область редактирования
+echo '<p><textarea id="ContentArea" name="areac">';
+require $c_NameArt.".html";
+//echo $c_NameArt.".html";
+echo '</textarea></p>';
+echo '<p><input id="InputArea" type="submit" value="Отправить"></p>';
+?>
 <!-- 
 </form>
 -->
-
 <?php
 // ****************************************************************************
 // *            Вывести (определить HTML-разметку) одну карточку галереи      *
@@ -131,22 +145,23 @@ function GViewImage($FileName,$Comment,$AreaText=false,$Action='Image')
 }
 function GLoadImage($FileName,$Comment,$AreaText=false,$Action='Image')
 {
+   echo '
+      <div class="Card">
+      <button class="bCard" type="submit" name="upload" value="'.
+      $FileName.'>';
+   echo '
+      <input type="hidden" name="MAX_FILE_SIZE" value="57200">
+      <input type="file" name="image" id="image">
+      <img class="imgCard" src="EdisiteArticle/_Edit/ImgTemplate.jpg" alt="'.
+      $FileName.'>';
+   echo '
+      </button>
+      <textarea class="taCard" name="aream">Текст комментария к картинке</textarea>
+      </div>';
    ?>
-   <!-- -->
-   <div class="Card">
-   <button class="bCard" type="submit" name="upload" value="FileName">
-   <input type="hidden" name="MAX_FILE_SIZE" value="57200">
-   <input type="file" name="image" id="image">
-   <img class="imgCard" src="EdisiteArticle/_Edit/ImgTemplate.jpg" alt="FileName">
    <!-- 
    <input type="submit" name="upload" id="upload" value="Upload">
    -->
-   </button>
-   <textarea class="taCard" name="aream">Текст комментария к картинке</textarea>
-   </div>
    <?php
 }
-
-
-
-// *********************************************************** EditText.php ***
+// *** <!-- --> **************************************** EdisiteArticle.php ***
--- www/EdisiteArticle/_Edit/GalleryNews.php
+++ www/EdisiteArticle/_Edit/GalleryNews.php
@@ -7,26 +7,14 @@
 
 //                                                   Автор:       Труфанов В.Е.
 //                                                   Дата создания:  13.01.2019
-// Copyright © 2019 tve                              Посл.изменение: 03.05.2021
-
-//echo '<form id="fGallery" action="'.$SpecSite.'">';
-echo '<form action="" method="post" enctype="multipart/form-data" id="uploadiImage">';
+// Copyright © 2019 tve                              Посл.изменение: 04.05.2021
 
 $FileName="EdisiteArticle/_Current/Подъем настроения.jpg";
 $Comment="Ночная прогулка по Ладоге до рассвета и подъёма настроения.";
 GViewImage($FileName,$Comment);
 
-$FileName="ittveNews/ittve01-001-С-заботой-и-к-мамам.jpg";
-$Comment="'С заботой и к мамам' - такой мамочкин хвостик.";
-GLoadImage($FileName,$Comment,true,"Upload");
-
 $FileName="EdisiteArticle/_Current/С заботой и к мамам.jpg";
 $Comment="'С заботой и к мамам' - такой мамочкин хвостик.";
 GViewImage($FileName,$Comment);
-
-echo '</form>';
-
-// Из галереи задаем режим представления выбранной картинки - "на высоту страницы"
-//$s_ModeImg=prown\MakeSession('ModeImg',vimOnPage,tInt);           
 
 // <!-- --> *********************************************** GalleryNews.php ***
