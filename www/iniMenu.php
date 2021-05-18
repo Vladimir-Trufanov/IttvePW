6479dfbd15440b74b8c13730f1df152fbe8730b7
Vladimir-Trufanov <tve@karelia.ru>
2021-05-18 21:19:48 +0300

    Неудачный алгоритм формирования тегов меню. Переделать.

--- www/iniMenu.php
+++ www/iniMenu.php
@@ -19,6 +19,7 @@ define ("ia",'<a ');
 define ("ea",'</a>'); 
 define ("ihref",'href="?com=');        
 define ("ehref",'">'); 
+define ("ibr",''); 
 */
 // ------------------------ Отладочный режим формирования тегов списка меню ---
 define ("ili",'(li)');        
@@ -30,6 +31,7 @@ define ("ea",'(/a)');
 define ("ihref",'href="');        
 define ("com",'?com='); 
 define ("ehref",'")'); 
+define ("ibr",'<br>'); 
 
 // ****************************************************************************
 // *  Сформировать подстроку из трех пробелов для каждого уровня со второго   *
@@ -50,20 +52,54 @@ function MakeSpases($level)
    return $Result;
 }
 // ****************************************************************************
+// *                         Если новый уровень меньше старого,               *
+// *                 то сформировать все /ul до совмещения уровней            *
+// ****************************************************************************
+function MakeEul($level,$levelOldi,&$isUL)
+{
+   $i=-9;
+   // Формируем трассировочный текст при отладке
+   $c='$level='.strval($level).' '.'$levelOldi='.strval($levelOldi);
+   $Result='';
+   if ($level>0)
+   {
+      while ($level<=$levelOldi) 
+      {
+         // Пересчитываем переменную, 
+         // чтобы определить условие для разблокировки вывода eul
+         $i=$level-$levelOldi;
+         //if (!$i==0) $Result=$Result.MakeSpases($levelOldi).'***'.strval($i).'***'.eul;
+         if (!$i==0) $Result=$Result.MakeSpases($levelOldi).eul.MakeSpases($levelOldi-1).eli;
+         $levelOldi--;
+      }
+      if ($i==-9) $Result='';
+      else if ($level=$levelOldi) $Result='';
+      else $isUL=true;
+      //else $Result=$Result.'...............'.$c.'***'.strval($i).'***';
+   }
+   return $Result;
+}
+// ****************************************************************************
 // *                               Сформировать меню                          *
 // ****************************************************************************
 function aViewMenu($array)
 {
    $Result='';
    $levelOld=0;
+   $isUL=true;
    // Выводим данные
    foreach ($array as $value)
    {
       // Строку с названием сайта = нулевой уровень не обрабатываем
       if (!$value['level']==0)
       {
+         /*
+         // Если новый уровень меньше старого, то формируем /ul
+         $Result=$Result.MakeEul($value['level'],$levelOld);
          // Формируем смещение для пункта меню
          $Result=$Result.MakeSpases($value['level']); 
+         // Если новый уровень больше старого, то формируем ul
+         if (($value['level']>$levelOld)and($levelOld>0)) $Result=$Result.iul.MakeSpases($value['level']);
          // Формируем li
          if ($value['IdCue']==tisRazdel)
          {
@@ -75,8 +111,31 @@ function aViewMenu($array)
             // Формируем обычную li пункта меню c материалом
             $Result=$Result.ili.ia.ihref.com.$value['Translit'].ehref.$value['Name'].ea.eli;
          }
-         //$Result=$Result.strval($value['IdCue']).' '.strval($value['level']).
-         //   ' -'.$value['Translit'].'='.$value['Name'].'-)<br>';
+         */
+
+         // Если новый уровень меньше старого, то формируем /ul
+         $Result=$Result.MakeEul($value['level'],$levelOld,$isUL);
+         // Формируем смещение для пункта меню
+         $Result=$Result.MakeSpases($value['level']); 
+         // Если новый уровень больше старого, то формируем ul
+         if (($value['level']>$levelOld)and($levelOld>0)) 
+         {
+            $Result=$Result.iul.MakeSpases($value['level']);
+            $isUL=false;
+         }
+         // Формируем li
+         if ($value['IdCue']==tisRazdel)
+         {
+            // Формируем li пункта меню с разделом
+            $Result=$Result.ili.ia.ihref."/".ehref.$value['Name'].ea;
+         }
+         else
+         {
+            // Формируем обычную li пункта меню c материалом
+            $Result=$Result.ili.ia.ihref.com.$value['Translit'].ehref.$value['Name'].ea;
+         }
+         if ($isUL) $Result=$Result.ibr;
+         else $Result=$Result.eli;
          // Запоминаем текущий обработанный уровень
          $levelOld=$value['level'];
       }
