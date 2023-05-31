e8c3a44c01757691603713df176911630975e231
Труфанов Владимир Евгеньевич <tve@karelia.ru>
2023-05-31 15:52:43 +0300

    Подключение к платформе Интернета вещей Rightech IoT

--- www/2023-05-30 Espres_esp8266ex/101-hello/101-hello.ino
+++ www/2023-05-30 Espres_esp8266ex/101-hello/101-hello.ino
@@ -1,7 +1,25 @@
+/*
+ * Таков был запуск моей модели esp01-tve58 на платформе Интернета вещей 31.05.2023:
+ * https://dev.rightech.io/#?n=root&v=map&m=objects&p=journal-v3&id=6476f06d23ae4a72e893ba2d&t=ric-handler-v2
+ * 
+ * https://dev.rightech.io       - подключение к платформе Интернета вещей Rightech IoT
+ * E-mail/логин                  - tve58@inbox.ru
+ * Пароль                        - Esp8266-tve
+ * Проект                        - Project of tve58@inbox.ru
+ * Объект (идентификатор модели) - esp01-tve58
+ * Имя модели                    - tve58-esp01-test
+ * 
+ * Имя                     - tve
+ * Телефон                 - 89214524295
+ * Организация             - Vidan17B
+ * 
+ */
+
 
 #include "Arduino.h"
-#include "EspMQTTClient.h" /* https://github.com/plapointe6/EspMQTTClient */
-                           /* https://github.com/knolleary/pubsubclient */
+#include "EspMQTTClient.h" 
+/* https://github.com/plapointe6/EspMQTTClient */
+/* https://github.com/knolleary/pubsubclient */
 #define PUB_DELAY (5 * 1000) /* 5 seconds */
 
 EspMQTTClient client(
@@ -13,11 +31,11 @@ EspMQTTClient client(
 );
 
 void setup() {
-  Serial.begin(9600);  
+  Serial.begin(9600);
 }
 
 void onConnectionEstablished() {
-  client.subscribe("base/relay/led1", [] (const String &payload)  {
+  client.subscribe("base/relay/led1", [] (const String & payload)  {
     Serial.println(payload);
   });
 }
